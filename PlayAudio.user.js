45625062668c7f4e6d65720250de16684d5e095e
poleshkoroman <romanpolesko@gmail.com>
2019-02-03 20:52:06 +0300

    leha 7

--- PlayAudio.user.js
+++ PlayAudio.user.js
@@ -9,12 +9,19 @@
 // @match        https://vk.com/audios194032004
 // ==/UserScript==
 
-var inline_src = (<><![CDATA[
-	import * from '../my_projects/smart_house_v0.0.1/src/App';
-    alert(recognizer);
-	let click = new Event('click');
-	document.getElementsByClassName('_audio_row__play_btn')[0].dispatchEvent(click);
 
-]]></>).toString();
-var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
-eval(c.code);
+(function() {
+    var app = require("../my_projects/smart_house_v0.0.1/src/App");
+    alert(recognizer);
+})();
+
+// var inline_src = (<><![CDATA[
+// 	// import * from "../my_projects/smart_house_v0.0.1/src/App";
+// 	var app = require("../my_projects/smart_house_v0.0.1/src/App");
+//     alert(recognizer);
+// 	let click = new Event('click');
+// 	document.getElementsByClassName('_audio_row__play_btn')[0].dispatchEvent(click);
+
+// ]]></>).toString();
+// var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
+// eval(c.code);
