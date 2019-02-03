c7b1888145500cebc83ea535a41da97e7444a132
poleshkoroman <romanpolesko@gmail.com>
2019-02-04 00:54:03 +0300

    add voiceassistant

---
+++ VoiceAssistant.js
@@ -0,0 +1,142 @@
+(function() {
+
+    let data = [
+		{
+			questions: "Лёха",
+			answer: "sho?",
+			do: "again"
+		},
+		{
+			questions: "Здорово",
+			answer: "darov",
+			do: "again"
+		},
+		{
+			questions: "Включи музыку",
+			answer: "kakuy",
+			do: "again"
+		},
+		{
+			questions: "YouTube",
+			answer: "sek",
+			do: "youtube"
+		},
+		{
+			questions: "ВК",
+			answer: "sek",
+			do: "music"
+		},
+		{
+			questions: "красавчик",
+			answer: "pasiba",
+			do: "again"
+		},
+		{
+			questions: "Спасибо",
+			answer: "ne za chto braatan",
+			do: "again"
+		},
+		{
+			questions: "что по погоде",
+			answer: "sek",
+			do: "weather"
+		},
+		{
+			questions: "погода",
+			answer: "sek",
+			do: "weather"
+		},
+		{
+			questions: "переводчик",
+			answer: "sek",
+			do: "translate"
+		},
+		{
+			questions: "Как дела",
+			answer: "kak vsegda za ebis",
+			do: "again"
+		},
+		{
+			questions: "понял",
+			answer: "krasava",
+			do: "again"
+		},
+		{
+			questions: "вырубай",
+			answer: "bb",
+			do: "off"
+		},
+		{
+			questions: "ничего",
+			answer: "kek",
+			do: "again"
+		},
+		{
+			answer: "ne ponyal",
+			do: "again"
+		}
+	]
+
+	const recognizer = new window.webkitSpeechRecognition();
+	recognizer.interimResults = true;
+	recognizer.lang = 'ru-Ru';
+
+	const speech = () => {
+		recognizer.onresult = (event, recognizer) => {
+		    const result = event.results[event.resultIndex];
+		    if (result.isFinal) {
+		   		caseEvent(result[0].transcript, recognizer);
+			}
+	  	};
+	};
+	const caseEvent = (text) => {
+		console.log(text);
+		data.forEach((item) => {
+			if (item.questions === text) {
+				const synth = window.speechSynthesis;
+	  			const utterance = new SpeechSynthesisUtterance(item.answer);
+	  			synth.speak(utterance);
+		  		doSomething(item.do);
+			}
+		})
+	};
+	const doSomething = (dosomething) => {
+		switch (dosomething) {
+			case "again" : {
+				this.speech();
+				break;
+			}
+			case "music" : {
+				window.open("https:\/\/vk.com\/audios194032004");
+				break;
+			}
+			case "off" : {
+				recognizer.removeEventListener('end', this.func);
+			  	break;
+			}
+			case "weather" : {
+				window.open("https:\/\/www.gismeteo.by/");
+				break;
+			}
+			case "translate" : {
+				window.open("https:\/\/translate.google.by/");
+				break;
+			}
+			case "youtube" : {
+				window.open("https:\/\/www.youtube.com/");
+				break;
+			}
+		}
+	}; 
+		
+	func() {
+		recognizer.start();	
+	};
+
+	document.addEventListener('DOMContentLoaded', () => {
+		recognizer.start();
+		speech();
+	  	recognizer.addEventListener('end', func);
+	});
+
+})();
