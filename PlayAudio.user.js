565c691309366b070ac754a3efa52e1a79cbbbbf
poleshkoroman <romanpolesko@gmail.com>
2019-02-03 15:57:02 +0300

    kek

---
+++ PlayAudio.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         PlayAudio
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  play audio in vk
+// @author       Roman Poleshko
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
+// @match        https://vk.com/audios194032004
+// ==/UserScript==
+
+var inline_src = (<><![CDATA[
+    alert('hello');
+]]></>).toString();
+var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
+eval(c.code);
