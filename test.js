3ae3458ece41a0d71fcbb598857a95851b7ba3fd
poleshkoroman <romanpolesko@gmail.com>
2019-02-04 01:19:16 +0300

    add test7

--- test.js
+++ test.js
@@ -1,4 +1,82 @@
 (function() {
+
+	var data = [
+		{
+			questions: "Лёха",
+			answer: "sho?",
+			do: "again"
+		},
+		{
+			questions: "Здорово",
+			answer: "darov",
+			do: "again"
+		},
+		{
+			questions: "Включи музыку",
+			answer: "kakuy",
+			do: "again"
+		},
+		{
+			questions: "YouTube",
+			answer: "sek",
+			do: "youtube"
+		},
+		{
+			questions: "ВК",
+			answer: "sek",
+			do: "music"
+		},
+		{
+			questions: "красавчик",
+			answer: "pasiba",
+			do: "again"
+		},
+		{
+			questions: "Спасибо",
+			answer: "ne za chto braatan",
+			do: "again"
+		},
+		{
+			questions: "что по погоде",
+			answer: "sek",
+			do: "weather"
+		},
+		{
+			questions: "погода",
+			answer: "sek",
+			do: "weather"
+		},
+		{
+			questions: "переводчик",
+			answer: "sek",
+			do: "translate"
+		},
+		{
+			questions: "Как дела",
+			answer: "kak vsegda za ebis",
+			do: "again"
+		},
+		{
+			questions: "понял",
+			answer: "krasava",
+			do: "again"
+		},
+		{
+			questions: "вырубай",
+			answer: "bb",
+			do: "off"
+		},
+		{
+			questions: "ничего",
+			answer: "kek",
+			do: "again"
+		},
+		{
+			answer: "ne ponyal",
+			do: "again"
+		}
+	]
+
 	var recognizer = new webkitSpeechRecognition();
 	recognizer.interimResults = true;
 	recognizer.lang = 'ru-Ru';
