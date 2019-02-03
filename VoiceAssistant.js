80e3410e678038e1765db2153d81651d9fad3374
poleshkoroman <romanpolesko@gmail.com>
2019-02-04 01:16:07 +0300

    add test4

--- VoiceAssistant.js
+++ VoiceAssistant.js
@@ -1,106 +1,147 @@
 (function() {
 
-    let data = [
-		{
-			questions: "Лёха",
-			answer: "sho?",
-			do: "again"
-		},
-		{
-			questions: "Здорово",
-			answer: "darov",
-			do: "again"
-		},
-		{
-			questions: "Включи музыку",
-			answer: "kakuy",
-			do: "again"
-		},
-		{
-			questions: "YouTube",
-			answer: "sek",
-			do: "youtube"
-		},
-		{
-			questions: "ВК",
-			answer: "sek",
-			do: "music"
-		},
-		{
-			questions: "красавчик",
-			answer: "pasiba",
-			do: "again"
-		},
-		{
-			questions: "Спасибо",
-			answer: "ne za chto braatan",
-			do: "again"
-		},
-		{
-			questions: "что по погоде",
-			answer: "sek",
-			do: "weather"
-		},
-		{
-			questions: "погода",
-			answer: "sek",
-			do: "weather"
-		},
-		{
-			questions: "переводчик",
-			answer: "sek",
-			do: "translate"
-		},
-		{
-			questions: "Как дела",
-			answer: "kak vsegda za ebis",
-			do: "again"
-		},
-		{
-			questions: "понял",
-			answer: "krasava",
-			do: "again"
-		},
-		{
-			questions: "вырубай",
-			answer: "bb",
-			do: "off"
-		},
-		{
-			questions: "ничего",
-			answer: "kek",
-			do: "again"
-		},
-		{
-			answer: "ne ponyal",
-			do: "again"
-		}
-	]
+ //    let data = [
+	// 	{
+	// 		questions: "Лёха",
+	// 		answer: "sho?",
+	// 		do: "again"
+	// 	},
+	// 	{
+	// 		questions: "Здорово",
+	// 		answer: "darov",
+	// 		do: "again"
+	// 	},
+	// 	{
+	// 		questions: "Включи музыку",
+	// 		answer: "kakuy",
+	// 		do: "again"
+	// 	},
+	// 	{
+	// 		questions: "YouTube",
+	// 		answer: "sek",
+	// 		do: "youtube"
+	// 	},
+	// 	{
+	// 		questions: "ВК",
+	// 		answer: "sek",
+	// 		do: "music"
+	// 	},
+	// 	{
+	// 		questions: "красавчик",
+	// 		answer: "pasiba",
+	// 		do: "again"
+	// 	},
+	// 	{
+	// 		questions: "Спасибо",
+	// 		answer: "ne za chto braatan",
+	// 		do: "again"
+	// 	},
+	// 	{
+	// 		questions: "что по погоде",
+	// 		answer: "sek",
+	// 		do: "weather"
+	// 	},
+	// 	{
+	// 		questions: "погода",
+	// 		answer: "sek",
+	// 		do: "weather"
+	// 	},
+	// 	{
+	// 		questions: "переводчик",
+	// 		answer: "sek",
+	// 		do: "translate"
+	// 	},
+	// 	{
+	// 		questions: "Как дела",
+	// 		answer: "kak vsegda za ebis",
+	// 		do: "again"
+	// 	},
+	// 	{
+	// 		questions: "понял",
+	// 		answer: "krasava",
+	// 		do: "again"
+	// 	},
+	// 	{
+	// 		questions: "вырубай",
+	// 		answer: "bb",
+	// 		do: "off"
+	// 	},
+	// 	{
+	// 		questions: "ничего",
+	// 		answer: "kek",
+	// 		do: "again"
+	// 	},
+	// 	{
+	// 		answer: "ne ponyal",
+	// 		do: "again"
+	// 	}
+	// ]
 
-	const recognizer = new window.webkitSpeechRecognition();
+	// const recognizer = new window.webkitSpeechRecognition();
+	// recognizer.interimResults = true;
+	// recognizer.lang = 'ru-Ru';
+
+	// const speech = () => {
+	// 	recognizer.onresult = (event, recognizer) => {
+	// 	    const result = event.results[event.resultIndex];
+	// 	    if (result.isFinal) {
+	// 	   		caseEvent(result[0].transcript, recognizer);
+	// 		}
+	//   	};
+	// };
+	// const caseEvent = (text) => {
+	// 	console.log(text);
+	// 	data.forEach((item) => {
+	// 		if (item.questions === text) {
+	// 			const synth = window.speechSynthesis;
+	//   			const utterance = new SpeechSynthesisUtterance(item.answer);
+	//   			synth.speak(utterance);
+	// 	  		doSomething(item.do);
+	// 		}
+	// 	})
+	// };
+	// const doSomething = (dosomething) => {
+	// 	switch (dosomething) {
+	// 		case "again" : {
+	// 			this.speech();
+	// 			break;
+	// 		}
+	// 		case "music" : {
+	// 			window.open("https:\/\/vk.com\/audios194032004");
+	// 			break;
+	// 		}
+	// 		case "off" : {
+	// 			recognizer.removeEventListener('end', this.func);
+	// 		  	break;
+	// 		}
+	// 		case "weather" : {
+	// 			window.open("https:\/\/www.gismeteo.by/");
+	// 			break;
+	// 		}
+	// 		case "translate" : {
+	// 			window.open("https:\/\/translate.google.by/");
+	// 			break;
+	// 		}
+	// 		case "youtube" : {
+	// 			window.open("https:\/\/www.youtube.com/");
+	// 			break;
+	// 		}
+	// 	}
+	// };
+
+	var recognizer = new webkitSpeechRecognition();
 	recognizer.interimResults = true;
 	recognizer.lang = 'ru-Ru';
-
-	const speech = () => {
-		recognizer.onresult = (event, recognizer) => {
-		    const result = event.results[event.resultIndex];
+	function speech(){
+		recognizer.onresult = function (event) {
+		    var result = event.results[event.resultIndex];
 		    if (result.isFinal) {
-		   		caseEvent(result[0].transcript, recognizer);
-			}
-	  	};
-	};
-	const caseEvent = (text) => {
-		console.log(text);
-		data.forEach((item) => {
-			if (item.questions === text) {
-				const synth = window.speechSynthesis;
-	  			const utterance = new SpeechSynthesisUtterance(item.answer);
-	  			synth.speak(utterance);
-		  		doSomething(item.do);
-			}
-		})
-	};
-	const doSomething = (dosomething) => {
+		      caseEvent(result[0].transcript, recognizer);
+		    } 
+		};
+	}; 
+
+	function doSomething(dosomething){
 		switch (dosomething) {
 			case "again" : {
 				this.speech();
@@ -111,7 +152,7 @@
 				break;
 			}
 			case "off" : {
-				recognizer.removeEventListener('end', this.func);
+				recognizer.removeEventListener('end', func);
 			  	break;
 			}
 			case "weather" : {
@@ -127,7 +168,19 @@
 				break;
 			}
 		}
-	}; 
+	};
+
+	function caseEvent (text) {
+		console.log(text);
+		data.forEach(function(item){
+			if (item.questions === text) {
+				var synth = window.speechSynthesis;
+	  			var utterance = new SpeechSynthesisUtterance(item.answer);
+	  			synth.speak(utterance);
+		  		doSomething(item.do);
+			}
+		})
+	};
 		
 	func() {
 		recognizer.start();	
