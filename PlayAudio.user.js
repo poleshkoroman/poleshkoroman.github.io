e5e2c61d9e77485efff89bcfc817ae34d5782f41
poleshkoroman <romanpolesko@gmail.com>
2019-02-04 10:29:37 +0300

    add test20

--- PlayAudio.user.js
+++ PlayAudio.user.js
@@ -97,7 +97,7 @@ var inline_src = (<><![CDATA[
 	recognizer.lang = 'ru-Ru';
 
 	const speech = () => {
-		recognizer.onresult = (event, recognizer) => {
+		recognizer.onresult = (event) => {
 		    const result = event.results[event.resultIndex];
 		    if (result.isFinal) {
 		   		caseEvent(result[0].transcript, recognizer);
@@ -105,7 +105,6 @@ var inline_src = (<><![CDATA[
 	  	};
 	};
 	const caseEvent = (text) => {
-		console.log(text);
 		data.forEach((item) => {
 			if (item.questions === text) {
 				const synth = speechSynthesis;
@@ -143,71 +142,15 @@ var inline_src = (<><![CDATA[
 			}
 		}
 	};
-
-	// var recognizer = new webkitSpeechRecognition();
-	// recognizer.interimResults = true;
-	// recognizer.lang = 'ru-Ru';
-	// function speech(){
-	// 	recognizer.onresult = function (event) {
-	// 	    var result = event.results[event.resultIndex];
-	// 	    if (result.isFinal) {
-	// 	      caseEvent(result[0].transcript, recognizer);
-	// 	    } 
-	// 	};
-	// }; 
-
-	// function doSomething(dosomething){
-	// 	switch (dosomething) {
-	// 		case "again" : {
-	// 			this.speech();
-	// 			break;
-	// 		}
-	// 		case "music" : {
-	// 			window.open("https:\/\/vk.com\/audios194032004");
-	// 			break;
-	// 		}
-	// 		case "off" : {
-	// 			recognizer.removeEventListener('end', func);
-	// 		  	break;
-	// 		}
-	// 		case "weather" : {
-	// 			window.open("https:\/\/www.gismeteo.by/");
-	// 			break;
-	// 		}
-	// 		case "translate" : {
-	// 			window.open("https:\/\/translate.google.by/");
-	// 			break;
-	// 		}
-	// 		case "youtube" : {
-	// 			window.open("https:\/\/www.youtube.com/");
-	// 			break;
-	// 		}
-	// 	}
-	// };
-
-	// function caseEvent (text) {
-	// 	console.log(text);
-	// 	for (var i = 0; i < data.length; i++) {
-	// 		var item = data[i];
-	// 		if (item.questions === text) {
-	// 			var synth = window.speechSynthesis;
-	//   			var utterance = new SpeechSynthesisUtterance(item.answer);
-	//   			synth.speak(utterance);
-	// 	  		doSomething(item.do);
-	// 		}
-	// 	}
-			
-	// };
 		
 	const func = () => {
 		recognizer.start();	
 	};
 
-	document.addEventListener('DOMContentLoaded', function(){
-		recognizer.start();
-		speech();
-	  	recognizer.addEventListener('end', func);
-	});
+	
+	recognizer.start();
+	speech();
+	recognizer.addEventListener('end', func);
 
 ]]></>).toString();
 var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
