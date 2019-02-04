5a5501d8cb8c32bae27b18f72a66d1906f91b698
poleshkoroman <romanpolesko@gmail.com>
2019-02-04 08:57:34 +0300

    add test16

--- PlayAudio.user.js
+++ PlayAudio.user.js
@@ -209,8 +209,6 @@ var inline_src = (<><![CDATA[
 	  	recognizer.addEventListener('end', func);
 	});
 
-}();
-
 ]]></>).toString();
 var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
 eval(c.code);
