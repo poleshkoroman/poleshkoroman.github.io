a0ac079a17d545952d057143c8897c0644857fcf
poleshkoroman <romanpolesko@gmail.com>
2019-02-12 20:38:57 +0300

    add weather

---
+++ CheckWeather.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         PlayAudio
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  play audio in vk
+// @author       Roman Poleshko
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
+// @match        https://www.gismeteo.by/
+// ==/UserScript==
+
+
+var inline_src = (<><![CDATA[
+	console.log('Hello from gismeteo');
+]]></>).toString();
+var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
+eval(c.code);
